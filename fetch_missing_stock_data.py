8ccf3b388f6afef97a414c6e3a9b72ccd2f72dc2
amau712 <amau712@gmail.com>
2026-04-30 15:59:43 +0800

    update

--- fetch_missing_stock_data.py
+++ fetch_missing_stock_data.py
@@ -13,10 +13,9 @@ def fetch_missing_stock_data(token: str, db_path: str = 'stock.db'):
 
     df = api.taiwan_stock_info()
     df = df[df["stock_id"].apply(lambda x: bool(re.fullmatch(r"[1-9]\d{3}", x)))]
-    df = df[df["date"] == df["date"].max()]
-    df = df.drop_duplicates(subset=["stock_id"])
     df = df[df["type"].isin(["twse", "tpex"])]
     df = df[df["industry_category"] != "存託憑證"]
+    df = df.sort_values("date").drop_duplicates(subset=["stock_id"], keep="last")
     df = df.sort_values(by="stock_id")
     stock_ids = df["stock_id"].tolist()
 
